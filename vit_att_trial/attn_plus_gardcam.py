bc2f43a7a44673b597fe9f19160a092303e1cf55
hagairavid18 <hagairavid18@gmail.com>
2022-01-21 11:44:36 +0200

    merged guy files

--- vit_att_trial/attn_plus_gardcam.py
+++ vit_att_trial/attn_plus_gardcam.py
@@ -194,7 +194,7 @@ for i, (images, labels) in enumerate(test_loader):
     cat, attn_map = generate_visualization(images)
     attn_diff_cls = []
     print('here2')
-    for j in range(1):
+    for j in range(4):
         attn_diff_cls.append(generate_visualization(images, class_index=j)[0])
     avg = attn_map.copy() * 6
     # print(avg.max())
@@ -229,11 +229,10 @@ for i, (images, labels) in enumerate(test_loader):
     img_buf = io.BytesIO()
     plt.savefig(img_buf, format='png')
     im = Image.open(img_buf)
-
+    #, wandb.Image(gradcam[2]),wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4]),
     row = [i, wandb.Image(images), label_names[predicted.item()], wandb.Image(im), label_names[labels.item()], T,
            wandb.Image(attn_diff_cls[0]), wandb.Image(attn_diff_cls[1]), wandb.Image(attn_diff_cls[2]),
-           wandb.Image(attn_diff_cls[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
-           wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4]), wandb.Image(avg)]
+           wandb.Image(attn_diff_cls[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[1]), wandb.Image(avg)]
     # test_dt.add_data(*row)
     # if i % 50 == 0:
     #     wandb.log({f"Grads_{name}_{i}": test_dt})
