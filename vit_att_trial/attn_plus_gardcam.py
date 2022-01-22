ecc96b21c07ce3105bacec9ddb7b7435b2e2e70d
hagairavid18 <hagairavid18@gmail.com>
2022-01-22 14:18:36 +0200

    multi model run

--- vit_att_trial/attn_plus_gardcam.py
+++ vit_att_trial/attn_plus_gardcam.py
@@ -26,6 +26,8 @@ from PIL import Image
 from baselines.ViT.ViT_LRP import vit_base_patch16_224 as vit_LRP
 from baselines.ViT.ViT_explanation_generator import LRP
 from pytorch_grad_cam.ablation_layer import AblationLayerVit
+from res_models import *
+
 
 
 seed = 25
@@ -38,7 +40,7 @@ random.seed(seed)
 os.environ['PYTHONHASHSEED'] = str(seed)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-wandb.init(project="test_attn_plus_gradcam")
+# wandb.init(project="test_attn_plus_gradcam")
 
 label_names = {
     0: "NORMAL",
@@ -82,8 +84,8 @@ attribution_generator = LRP(model_attn)
 
 pytorch_total_params = sum(p.numel() for p in model_timm.parameters())
 pytorch_total_params_train = sum(p.numel() for p in model_timm.parameters() if p.requires_grad)
-wandb.log({"Total Params": pytorch_total_params})
-wandb.log({"Trainable Params": pytorch_total_params_train})
+# wandb.log({"Total Params": pytorch_total_params})
+# wandb.log({"Trainable Params": pytorch_total_params_train})
 
 
 def generate_visualization(original_image, class_index=None):
@@ -128,146 +130,161 @@ total_arr = [0.0] * 10
 predictions = None
 ground_truth = None
 # Iterate through test dataset
-#  'XGradCAM', 'EigenGradCAM',
-columns = ["id", "Original Image", "Predicted", "Logits", "Truth", "Correct","curr_target", "Attention",
-           "GradCAM",'EigenCAM', 'GradCAMPlusPlus'] + ["level {}".format(i) for i in range(0,len(model_timm.blocks),2)] + ['Avg']
-# for a in label_names:
-#     columns.append("score_" + a)
-test_dt = wandb.Table(columns=columns)
+#  'XGradCAM', 'EigenGradCAM','EigenCAM', 'GradCAMPlusPlus'
 
-for i, (images, labels) in enumerate(test_loader):
-    if i % 10 == 0:
-        print(f'image : {i}\n\n\n')
-    images = Variable(images).to(device)
-    labels = labels.to(device)
-    images = images.squeeze()
-    # Forward pass only to get logits/output
-    # print(images.shape)
-    outputs_attn = model_attn(images.unsqueeze(0))
-    outputs_timm = model_attn(images.unsqueeze(0))
+names = ["res18", 'vit_base_patch16_224']
+models = [Resnet18(4),model_timm ]
+for name, model in zip(names, models):
+    print(name)
+    if name != 'vit_base_patch16_224':
+        model.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
+        model = model.to(device)
+    correct = 0.0
+    correct_arr = [0.0] * 10
+    total = 0.0
+    total_arr = [0.0] * 10
+    predictions = None
+    ground_truth = None
+    # Iterate through test dataset
+    for i, (images, labels) in enumerate(test_loader):
+        if i % 10 == 0:
+            print(f'image : {i}\n\n\n')
+        images = Variable(images).to(device)
+        labels = labels.to(device)
+        # Forward pass only to get logits/output
+        # print(images.shape)
+        if name == 'vit_base_patch16_224':
+            outputs_attn = model_attn(images)
+        outputs = model(images)
 
-    # Get predictions from the maximum value
-    _, predicted = torch.max(outputs_attn.data, 1)
+        # Get predictions from the maximum value
+        _, predicted = torch.max(outputs.data, 1)
 
-    # Total number of labels
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
+        # Total number of labels
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
 
-    for label in range(4):
-        correct_arr[label] += (((predicted == labels) & (labels == label)).sum())
-        total_arr[label] += (labels == label).sum()
+        for label in range(4):
+            correct_arr[label] += (((predicted == labels) & (labels == label)).sum())
+            total_arr[label] += (labels == label).sum()
 
-    if i == 0:
-        predictions = predicted
-        ground_truth = labels
-    else:
-        predictions = torch.cat((predictions, predicted), 0)
-        ground_truth = torch.cat((ground_truth, labels), 0)
+        if i == 0:
+            predictions = predicted
+            ground_truth = labels
+        else:
+            predictions = torch.cat((predictions, predicted), 0)
+            ground_truth = torch.cat((ground_truth, labels), 0)
 
-    target_layers = [model_timm.blocks[-1].norm1]
-    # , ScoreCAM, EigenCAM, GradCAMPlusPlus, XGradCAM, EigenGradCAM
-    cams = [GradCAM, EigenCAM, GradCAMPlusPlus]
+        target_layers = [model_timm.blocks[-1].norm1]
+        # , ScoreCAM, EigenCAM, GradCAMPlusPlus, XGradCAM, EigenGradCAM
+        cams = [GradCAM]
 
-    images = images.unsqueeze(0)
-    image_transformer_attribution = None
-    for k in range(4):
-        just_grads = []
-        res = []
-        attn_diff_cls = []
+        # images = images.unsqueeze(0)
+        image_transformer_attribution = None
+        for k in range(4):
+            print("curr label: {}".format(k))
+            just_grads = []
+            res = []
+            attn_diff_cls = []
 
-        target_categories = [k]
-        targets = [ClassifierOutputTarget(category) for category in target_categories]
+            target_categories = [k]
+            targets = [ClassifierOutputTarget(category) for category in target_categories]
 
-        for cam_algo in cams:
-            # print(images.shape)
+            for cam_algo in cams:
+                # print(images.shape)
 
-            cam = cam_algo(model=model_timm, target_layers=target_layers,
-                           use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform,
-                           )
-            target_category = labels.item()
-            grayscale_cam = cam(input_tensor=images, aug_smooth=True, eigen_smooth=True,targets=targets)
-            just_grads.append(grayscale_cam[0, :])
-            image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
-            image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
-                    image_transformer_attribution.max() - image_transformer_attribution.min())
-            vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[0, :])
-            vis = np.uint8(255 * vis)
-            vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
-            res.append(vis)  # superimposed_img / 255)
-        for level in range(0,len(model_timm.blocks),2):
-            target_layers = [model_timm.blocks[level].norm1]
+                cam = cam_algo(model=model_timm, target_layers=target_layers,
+                               use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform,
+                               )
+                target_category = labels.item()
+                grayscale_cam = cam(input_tensor=images, aug_smooth=True, eigen_smooth=True,targets=targets)
+                just_grads.append(grayscale_cam[0, :])
+                image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
+                image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
+                        image_transformer_attribution.max() - image_transformer_attribution.min())
+                vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[0, :])
+                vis = np.uint8(255 * vis)
+                vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
+                res.append(vis)  # superimposed_img / 255)
 
-            cam = GradCAM(model=model_timm, target_layers=target_layers,
-                           use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform,
-                           )
-            target_category = labels.item()
-            grayscale_cam = cam(input_tensor=images, aug_smooth=True, eigen_smooth=True, targets=targets)
-            # just_grads.append(grayscale_cam[0, :])
-            image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
-            image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
-                    image_transformer_attribution.max() - image_transformer_attribution.min())
-            vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[0, :])
-            vis = np.uint8(255 * vis)
-            vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
-            res.append(vis)  # superimposed_img / 255)
-
-
-
-        gradcam = res
-        # images = images.squeeze()
-        cat, attn_map = generate_visualization(images.squeeze())
+            # # layer by layer grad cam
+            # for level in range(0,len(model_timm.blocks),2):
+            #     target_layers = [model_timm.blocks[level].norm1]
+            #
+            #     cam = GradCAM(model=model_timm, target_layers=target_layers,
+            #                    use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform,
+            #                    )
+            #     target_category = labels.item()
+            #     grayscale_cam = cam(input_tensor=images, aug_smooth=True, eigen_smooth=True, targets=targets)
+            #     # just_grads.append(grayscale_cam[0, :])
+            #     image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
+            #     image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
+            #             image_transformer_attribution.max() - image_transformer_attribution.min())
+            #     vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[0, :])
+            #     vis = np.uint8(255 * vis)
+            #     vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
+            #     res.append(vis)  # superimposed_img / 255)
 
 
-        attention = generate_visualization(images.squeeze(), class_index=k)[0]
-        avg = attn_map.copy() * 6
-        # print(avg.max())
-        for j, grad in enumerate(just_grads):
-            g = grad.copy()
-            # plt.imshow(g)
-            # plt.title(str(j))
-            # plt.show()
-            g = np.where(g < g.max() / 4, g / 7, g)
-            g = np.exp(g)
-            g = g - g.min()
-            g = g / g.max()
-            # plt.imshow(g)
-            # plt.title(str(j))
-            # plt.show()
-            avg += g
+
+            gradcam = res
+            # images = images.squeeze()
+            attention = None
+            avg = just_grads[0].copy() * 0
+
+            if name == 'vit_base_patch16_224':
+                cat, attn_map = generate_visualization(images.squeeze())
+                attention = generate_visualization(images.squeeze(), class_index=k)[0]
+
+
+                avg = attn_map.copy() * 6
             # print(avg.max())
-        avg = avg / avg.max()
-        # plt.imshow(avg)
-        # plt.show()
-        vis = show_cam_on_image(image_transformer_attribution, avg)
-        vis = np.uint8(255 * vis)
-        vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
-        # plt.imshow(vis)
-        # plt.show()
-        avg = vis
-        T = predicted.item() == labels.item()
-        out = outputs_timm
+            for j, grad in enumerate(just_grads):
+                g = grad.copy()
+                # plt.imshow(g)
+                # plt.title(str(j))
+                # plt.show()
+                g = np.where(g < g.max() / 4, g / 7, g)
+                g = np.exp(g)
+                g = g - g.min()
+                g = g / g.max()
+                # plt.imshow(g)
+                # plt.title(str(j))
+                # plt.show()
+                avg += g
+                # print(avg.max())
+            avg = avg / avg.max()
+            # plt.imshow(avg)
+            # plt.show()
+            vis = show_cam_on_image(image_transformer_attribution, avg)
+            vis = np.uint8(255 * vis)
+            vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
+            # plt.imshow(vis)
+            # plt.show()
+            avg = vis
+            T = predicted.item() == labels.item()
+            # out = outputs_timm
+            #
+            # plt.bar(label_names, out.cpu().detach().numpy()[0])
+            # plt.xlabel(label_names)
+            img_buf = io.BytesIO()
+            plt.savefig(img_buf, format='png')
+            im = Image.open(img_buf)
 
-        plt.bar(label_names, out.cpu().detach().numpy()[0])
-        # plt.xlabel(label_names)
-        img_buf = io.BytesIO()
-        plt.savefig(img_buf, format='png')
-        im = Image.open(img_buf)
-        # , wandb.Image(gradcam[4]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
-        #            wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4])
-        row = [i, wandb.Image(images), label_names[predicted.item()], wandb.Image(im), label_names[labels.item()], T,
-               label_names[k],wandb.Image(attention)] + [wandb.Image(cam) for cam in gradcam ] +[wandb.Image(avg)]
-        test_dt.add_data(*row)
-    space_row = [None for _ in row]
-    test_dt.add_data(*space_row)
-    # if i % 50 == 0:
-    #     wandb.log({f"Grads_{name}_{i}": test_dt})
-    # wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None,
-    #                                                    y_true=ground_truth, preds=predictions,
-    #                                                    class_names=label_names)})
+            row = [i, wandb.Image(images), label_names[predicted.item()], wandb.Image(im), label_names[labels.item()], T,
+                   label_names[k]]+[wandb.Image(attention) if attention else None] + [wandb.Image(cam) for cam in gradcam ] +[wandb.Image(avg)]
+            # test_dt.add_data(*row)
+        space_row = [None for _ in row]
+        # test_dt.add_data(*space_row)
+        # if i % 50 == 0:
+        #     wandb.log({f"Grads_{name}_{i}": test_dt})
+        # wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None,
+        #                                                    y_true=ground_truth, preds=predictions,
+        #                                                    class_names=label_names)})
 
-accuracy = correct / total
-metrics = {f'Test Accuracy_{name}': accuracy}
-for label in range(4):
-    metrics[f'Test Accuracy_{name}' + label_names[label]] = correct_arr[label] / total_arr[label]
-wandb.log(metrics)
-wandb.log({f"Grads_{name}": test_dt})
+    accuracy = correct / total
+    metrics = {f'Test Accuracy_{name}': accuracy}
+    for label in range(4):
+        metrics[f'Test Accuracy_{name}' + label_names[label]] = correct_arr[label] / total_arr[label]
+    # wandb.log(metrics)
+    # wandb.log({f"Grads_{name}": test_dt})
