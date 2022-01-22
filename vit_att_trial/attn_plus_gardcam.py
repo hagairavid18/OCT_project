03d22dab14e7fc223bf4fc0a40fa3482cec1de0d
hagairavid18 <hagairavid18@gmail.com>
2022-01-22 16:22:32 +0200

    add convnext

--- vit_att_trial/attn_plus_gardcam.py
+++ vit_att_trial/attn_plus_gardcam.py
@@ -183,11 +183,13 @@ for name, model in zip(names, models):
         else:
             predictions = torch.cat((predictions, predicted), 0)
             ground_truth = torch.cat((ground_truth, labels), 0)
-        target_layers = [model.downsample_layers[-1]]
+
         if name =='res18':
             target_layers = [model.resnet.layer4[-1]]
         elif name == 'vit_base_patch16_224':
             target_layers = [model.blocks[-1].norm1]
+        else:
+            target_layers = [model.downsample_layers[-1]]
         # , ScoreCAM, EigenCAM, GradCAMPlusPlus, XGradCAM, EigenGradCAM
         cams = [GradCAM]
         print(target_layers[0])
