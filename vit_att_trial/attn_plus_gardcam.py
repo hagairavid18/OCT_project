9e281086b8909bdb24491c9d845efa409c4f432e
hagairavid18 <hagairavid18@gmail.com>
2022-01-23 10:41:26 +0200

    organize

--- vit_att_trial/attn_plus_gardcam.py
+++ vit_att_trial/attn_plus_gardcam.py
@@ -118,11 +118,12 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 if config['use_wandb']:
     test_dt = wandb.Table(columns=columns)
 
-
-names = ["res18","res50","res101","res152","convnext_xlarge", 'vit_base_patch16_224']
+#,"res50","res101","res152","convnext_xlarge",
+names = ["res18" 'vit_base_patch16_224']
 for i, (images, labels) in enumerate(test_loader):
 
-    for name, model in zip(names, models):
+    for name, i in enumerate(names):
+        model = models[i]
         print(name)
         if name != 'vit_base_patch16_224':
             model.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
