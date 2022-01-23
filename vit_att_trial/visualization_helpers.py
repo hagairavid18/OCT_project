a6b4dadc75ccbbf663228476d3a8300c97583b39
hagairavid18 <hagairavid18@gmail.com>
2022-01-23 15:33:22 +0200

    generate_cam_wis

--- vit_att_trial/visualization_helpers.py
+++ vit_att_trial/visualization_helpers.py
@@ -30,7 +30,6 @@ from pytorch_grad_cam.ablation_layer import AblationLayerVit
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def generate_cam_vis(model,cam_algo, target_layers,name,images,labels,targets):
-    print(target_layers)
     cam = cam_algo(model=model, target_layers=target_layers,
                    use_cuda=True if torch.cuda.is_available() else False)  # , reshape_transform=reshape_transform,
     # )
@@ -39,14 +38,13 @@ def generate_cam_vis(model,cam_algo, target_layers,name,images,labels,targets):
                        use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform,
                        )
     target_category = labels.item()
-    # print(cam.shape)
     grayscale_cam = cam(input_tensor=images, aug_smooth=True, eigen_smooth=True, targets=targets)
 
-    curr_grads= (grayscale_cam[i, :])
+    curr_grads= (grayscale_cam[0, :])
     image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
     image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
             image_transformer_attribution.max() - image_transformer_attribution.min())
-    vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[i, :])
+    vis = show_cam_on_image(image_transformer_attribution, grayscale_cam[0, :])
     vis = np.uint8(255 * vis)
     vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
 
