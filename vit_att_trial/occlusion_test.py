413eeebd034343ab217c8b3d35ce742ca06a5a8b
hagairavid18 <hagairavid18@gmail.com>
2022-02-08 16:09:15 +0200

    occlusion_test

--- vit_att_trial/occlusion_test.py
+++ vit_att_trial/occlusion_test.py
@@ -211,7 +211,7 @@ for i, (images, labels) in enumerate(test_loader):
         # Get predictions from the maximum value
         _, predictions = torch.max(outputs.data, 1)
         print(outputs)
-        print(torch.topk(2,outputs.data))
+        print(torch.topk(2,outputs))
         count += 1
 
         target_layers = [config[name]['target_layers'][-1]]
