b8ec1683adbafa8b1ea47baa844b6bfd9ebeed2a
hagairavid18 <hagairavid18@gmail.com>
2022-02-08 16:15:56 +0200

    occlusion_test

--- vit_att_trial/occlusion_test.py
+++ vit_att_trial/occlusion_test.py
@@ -214,7 +214,7 @@ for i, (images, labels) in enumerate(test_loader):
         print(torch.topk(k=2,input=outputs).values)
         print(torch.topk(k=2, input=outputs).values[0,0])
         print(torch.topk(k=2, input=outputs).values[0,1])
-        if print(torch.topk(k=2, input=outputs).values[0,0]) - print(torch.topk(k=2, input=outputs).values[0,1]) >1:
+        if torch.topk(k=2, input=outputs).values[0,0] - torch.topk(k=2, input=outputs).values[0,1] >1:
             break
         else:
             print('here')
