2c6bf8f218686b229008f7ed2bde6d8ea4a657a2
hagairavid18 <hagairavid18@gmail.com>
2022-02-08 10:55:41 +0200

    occlusion_test

--- vit_att_trial/occlusion_test.py
+++ vit_att_trial/occlusion_test.py
@@ -191,7 +191,7 @@ for i, (images, labels) in enumerate(test_loader):
 
         target_layers = [config[name]['target_layers'][-1]]
         # compute occlusion heatmap
-        heatmap = occlusion(model, images, predictions.item(), 50, 2)
+        heatmap = occlusion(model, images, predictions.item(), 50, 5)
 
         # displaying the image using seaborn heatmap and also setting the maximum value of gradient to probability
         # imgplot = sns.heatmap(heatmap, xticklabels=False, yticklabels=False, vmax=prob_no_occ)
