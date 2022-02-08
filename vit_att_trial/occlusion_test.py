d418af341f7c45777435edaa282a5aeabc501585
hagairavid18 <hagairavid18@gmail.com>
2022-02-08 18:31:16 +0200

    occlusion_test

--- vit_att_trial/occlusion_test.py
+++ vit_att_trial/occlusion_test.py
@@ -1,3 +1,4 @@
+from PIL.ImageColor import colormap
 from attn_data import Kermany_DataSet
 import timm
 import wandb
@@ -67,7 +68,10 @@ def occlusion(model, image, label, occ_size=100, occ_stride=100, occ_pixel=0.5):
 
             # run inference on modified image
             output = model(input_image)
-
+            # print(output)
+            output_prob = nn.functional.softmax(output, dim=1)
+            # print(output_prob)
+            # print(output_prob.tolist()[0][label])
             prob = output.tolist()[0][label]
             if prob>top_10_masks[-1][1]:
                 # print(max_prob)
@@ -80,30 +84,32 @@ def occlusion(model, image, label, occ_size=100, occ_stride=100, occ_pixel=0.5):
 
             # setting the heatmap location to probability value
             heatmap[h, w] = prob
-    image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
-    image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
-    image_transformer_attribution.max() - image_transformer_attribution.min())
+    # image_transformer_attribution = images.squeeze().permute(1, 2, 0).data.cpu().numpy()
+    # image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
+    # image_transformer_attribution.max() - image_transformer_attribution.min())
     # vis = np.uint8(255 * vis)
     # vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
+    print(heatmap.shape)
     heatmap = heatmap.permute(1, 0)
+    print(heatmap.shape)
+    print(heatmap.shape)
 
     heatmap = np.uint8(255 * heatmap)
 
     heatmap = heatmap - np.min(heatmap)
-    new_heatmap = heatmap / (1e-7 + np.max(heatmap))
-    new_heatmap = cv2.resize(new_heatmap,image_transformer_attribution.shape[:2])
+    # heatmap = heatmap / (1e-7 + np.max(heatmap))
+    print(image.shape)
+    # heatmap = cv2.resize(heatmap, )
+    print(image.shape)
 
     # result = np.float32(result)
-
-    vis = show_cam_on_image(image_transformer_attribution, np.resize(new_heatmap,(496,512,3)))
+    heatmap = cv2.applyColorMap(np.uint8(255 * heatmap), colormap)
+    # vis = show_cam_on_image(image_transformer_attribution, heatmap)
     # vis = show_cam_on_image(image_transformer_attribution, heatmap)
 
     # print(heatmap)
     masked_image = image.clone().detach()
-    heatmap = cv2.cvtColor(np.array(heatmap), cv2.COLOR_RGB2BGR)
-    print(heatmap.shape)
 
-    heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
     for tup in top_10_masks:
         # print(tup)
         w_start, w_end, h_start, h_end = tup[0]
@@ -111,7 +117,7 @@ def occlusion(model, image, label, occ_size=100, occ_stride=100, occ_pixel=0.5):
         masked_image[:, :, w_start:w_end, h_start:h_end] = occ_pixel
     output = model(masked_image)
     prob = output.tolist()[0][label]
-    return vis,heatmap,masked_image,output
+    return heatmap,masked_image,output
 
 seed = 25
 torch.manual_seed(hash("by removing stochasticity") % seed)
@@ -125,27 +131,27 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 
-# name = 'vit_base_patch16_224'
-# # initialize ViT pretrained
-# model_timm = timm.create_model(name, num_classes=4, img_size=(496, 512))
-# model_timm.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
-# model_timm = model_timm.to(device)
-#
-# model_attn = vit_LRP(num_classes=4, img_size=(496, 512))
-# model_attn.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
-# model_attn = model_attn.to(device)
-# model_attn.eval()
-# attribution_generator = LRP(model_attn)
+name = 'vit_base_patch16_224'
+# initialize ViT pretrained
+model_timm = timm.create_model(name, num_classes=4, img_size=(496, 512))
+model_timm.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
+model_timm = model_timm.to(device)
 
-models = [Resnet18(4),Resnet50(4),Resnet101(4),Resnet152(4),convnext_base() ]
+model_attn = vit_LRP(num_classes=4, img_size=(496, 512))
+model_attn.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
+model_attn = model_attn.to(device)
+model_attn.eval()
+attribution_generator = LRP(model_attn)
+
+models = [Resnet18(4),Resnet50(4),Resnet101(4),Resnet152(4),convnext_base(),model_timm ]
 
 config = {'res18':{'target_layers':[models[0].resnet.layer2[i] for i in range(0,len(models[0].resnet.layer2))]+[models[0].resnet.layer3[i] for i in range(0,len(models[0].resnet.layer3))]+[models[0].resnet.layer4[i] for i in range(len(models[0].resnet.layer4)-1,2)]+[models[0].resnet.layer4[-1]]},
           'res50':{'target_layers':[models[1].resnet.layer2[i] for i in range(0,len(models[1].resnet.layer2),2)]+[models[1].resnet.layer3[i] for i in range(0,len(models[1].resnet.layer3),2)]+[models[1].resnet.layer4[i] for i in range(len(models[1].resnet.layer4)-1,2)]+[models[1].resnet.layer4[-1]]},
           'res101':{'target_layers':[models[2].resnet.layer3[i] for i in range(0,len(models[2].resnet.layer3),3)]+[models[2].resnet.layer4[i] for i in range(len(models[2].resnet.layer4)-1,2)]+[models[2].resnet.layer4[-1]]},
           'res152':{'target_layers':[models[3].resnet.layer3[i] for i in range(0,len(models[3].resnet.layer3),5)]+[models[3].resnet.layer4[i] for i in range(len(models[3].resnet.layer4)-1,2)]+[models[3].resnet.layer4[-1]]},
           'convnext_xlarge':{'target_layers':[models[4].downsample_layers[0],models[4].downsample_layers[1],models[4].downsample_layers[-1]]},
-          # 'vit_base_patch16_224':{'target_layers':[models[5].blocks[i].norm1 for i in range(0,len(model_timm.blocks))]},
-          'use_wandb': True,
+          'vit_base_patch16_224':{'target_layers':[models[5].blocks[i].norm1 for i in range(0,len(model_timm.blocks))]},
+          'use_wandb': False,
           'visualize_all_class': False,
           'seed': 25,
           'test_path' :"../../data/kermany/test",
@@ -160,7 +166,7 @@ config = {'res18':{'target_layers':[models[0].resnet.layer2[i] for i in range(0,
 # , ScoreCAM, EigenCAM, GradCAMPlusPlus, XGradCAM, EigenGradCAM
 
 
-columns = ["id", "Original Image", "prediction" ,"Logits","Truth","curr_target",'GradCAM',"occlusion inter","occlusion","best_mask","Logits after","new prediction"]\
+columns = ["id", "Original Image", "prediction" ,"Logits","Truth","curr_target",'GradCAM',"occlusion","best_mask","Logits after","new prediction"]\
 
 
 if config['use_wandb']:
@@ -219,7 +225,7 @@ for i, (images, labels) in enumerate(test_loader):
 
     target_layers = [config[name]['target_layers'][-1]]
     # compute occlusion heatmap
-    inter_heatmap,heatmap,best_mask,new_ouputs = occlusion(model, images, labels.item(), 50, 20)
+    heatmap,best_mask,new_ouputs = occlusion(model, images, labels.item(), 200, 50)
     _, new_predictions = torch.max(new_ouputs.data, 1)
     # displaying the image using seaborn heatmap and also setting the maximum value of gradient to probability
     # imgplot = sns.heatmap(heatmap, xticklabels=False, yticklabels=False, vmax=prob_no_occ)
@@ -267,7 +273,7 @@ for i, (images, labels) in enumerate(test_loader):
         im_2 = Image.open(img_buf_2)
 
         row = [str(i), wandb.Image(images), config['label_names'][predictions.item()], wandb.Image(im), config['label_names'][labels.item()], T,
-               ]+[wandb.Image(gradcam[i]) for i in range(len(gradcam))]+[ wandb.Image(inter_heatmap),wandb.Image(heatmap),wandb.Image(best_mask),wandb.Image(im_2), config['label_names'][new_predictions.item()]]
+               ]+[wandb.Image(gradcam[i]) for i in range(len(gradcam))]+[ wandb.Image(heatmap),wandb.Image(best_mask),wandb.Image(im_2), config['label_names'][new_predictions.item()]]
         # row_2 = [  None for _ in range(len(config['vit_base_patch16_224']['target_layers']))]
         # for pos in range(len(layer_cam)):
         #     row_2[pos] = wandb.Image(layer_cam[pos])
