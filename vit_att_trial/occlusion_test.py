4ef692141eaecc51069e692bfffe7eae9ef82a72
hagairavid18 <hagairavid18@gmail.com>
2022-02-08 11:13:21 +0200

    occlusion_test

--- vit_att_trial/occlusion_test.py
+++ vit_att_trial/occlusion_test.py
@@ -105,26 +105,26 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 
-# name = 'vit_base_patch16_224'
-# # initialize ViT pretrained
-# model_timm = timm.create_model(name, num_classes=4, img_size=(496, 512))
-# model_timm.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
-# model_timm = model_timm.to(device)
-#
-# model_attn = vit_LRP(num_classes=4, img_size=(496, 512))
-# model_attn.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
-# model_attn = model_attn.to(device)
-# model_attn.eval()
-# attribution_generator = LRP(model_attn)
+name = 'vit_base_patch16_224'
+# initialize ViT pretrained
+model_timm = timm.create_model(name, num_classes=4, img_size=(496, 512))
+model_timm.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
+model_timm = model_timm.to(device)
 
-models = [Resnet18(4),Resnet50(4)]
+model_attn = vit_LRP(num_classes=4, img_size=(496, 512))
+model_attn.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
+model_attn = model_attn.to(device)
+model_attn.eval()
+attribution_generator = LRP(model_attn)
+
+models = [Resnet18(4),Resnet50(4),Resnet101(4),Resnet152(4),convnext_base(),model_timm ]
 
 config = {'res18':{'target_layers':[models[0].resnet.layer2[i] for i in range(0,len(models[0].resnet.layer2))]+[models[0].resnet.layer3[i] for i in range(0,len(models[0].resnet.layer3))]+[models[0].resnet.layer4[i] for i in range(len(models[0].resnet.layer4)-1,2)]+[models[0].resnet.layer4[-1]]},
           'res50':{'target_layers':[models[1].resnet.layer2[i] for i in range(0,len(models[1].resnet.layer2),2)]+[models[1].resnet.layer3[i] for i in range(0,len(models[1].resnet.layer3),2)]+[models[1].resnet.layer4[i] for i in range(len(models[1].resnet.layer4)-1,2)]+[models[1].resnet.layer4[-1]]},
-          # 'res101':{'target_layers':[models[2].resnet.layer3[i] for i in range(0,len(models[2].resnet.layer3),3)]+[models[2].resnet.layer4[i] for i in range(len(models[2].resnet.layer4)-1,2)]+[models[2].resnet.layer4[-1]]},
-          # 'res152':{'target_layers':[models[3].resnet.layer3[i] for i in range(0,len(models[3].resnet.layer3),5)]+[models[3].resnet.layer4[i] for i in range(len(models[3].resnet.layer4)-1,2)]+[models[3].resnet.layer4[-1]]},
-          # 'convnext_xlarge':{'target_layers':[models[4].downsample_layers[0],models[4].downsample_layers[1],models[4].downsample_layers[-1]]},
-          # 'vit_base_patch16_224':{'target_layers':[models[5].blocks[i].norm1 for i in range(0,len(model_timm.blocks))]},
+          'res101':{'target_layers':[models[2].resnet.layer3[i] for i in range(0,len(models[2].resnet.layer3),3)]+[models[2].resnet.layer4[i] for i in range(len(models[2].resnet.layer4)-1,2)]+[models[2].resnet.layer4[-1]]},
+          'res152':{'target_layers':[models[3].resnet.layer3[i] for i in range(0,len(models[3].resnet.layer3),5)]+[models[3].resnet.layer4[i] for i in range(len(models[3].resnet.layer4)-1,2)]+[models[3].resnet.layer4[-1]]},
+          'convnext_xlarge':{'target_layers':[models[4].downsample_layers[0],models[4].downsample_layers[1],models[4].downsample_layers[-1]]},
+          'vit_base_patch16_224':{'target_layers':[models[5].blocks[i].norm1 for i in range(0,len(model_timm.blocks))]},
           'use_wandb': True,
           'visualize_all_class': False,
           'seed': 25,
@@ -155,14 +155,14 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 
 
 #,"res50","res101","res152","convnext_xlarge", 'vit_base_patch16_224'
-names = ["res50"]
+names = ['convnext_xlarge']
 # predictions = None
 # ground_truth = None
 count = 0
 if config['use_wandb']:
     test_dt = wandb.Table(columns=columns)
 for i, (images, labels) in enumerate(test_loader):
-    if count == 5:
+    if count == 1:
         break
 
     images = Variable(images).to(device)
@@ -175,7 +175,7 @@ for i, (images, labels) in enumerate(test_loader):
     print(count)
     for index, name in enumerate(names):
 
-        model = models[1]
+        model = models[4]
         print(name)
         if name != 'vit_base_patch16_224':
             model.load_state_dict(torch.load(f'{name}.pt', map_location=torch.device(device)))
